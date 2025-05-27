519d6770f25587b8c16da7d0359a962213be909a
prabath1998 <prabhathudayanga1998@gmail.com>
2025-05-27 22:37:39 +0530

    integrate landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
 
     public function redirectAdmin()
     {
-        return view('welcome');
+        // return view('welcome');
 
         return redirect()->route('admin.dashboard');
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                     <ul class="navbar-nav ms-auto">
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('admin.login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
